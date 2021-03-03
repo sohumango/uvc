28d2baed2c28b0f9bff6b73f957134dcbfde11ea
wanghj <wang@normee.co.jp>
2021-03-03 11:48:29 +0900

    add demo work

---
+++ demouvc/app/src/main/java/jp/co/normee/demouvc/AspectRatioViewInterface.java
@@ -0,0 +1,7 @@
+package jp.co.normee.demouvc;
+
+interface AspectRatioViewInterface {
+    public void setAspectRatio(double aspectRatio);
+    public void onPause();
+    public void onResume();
+}
